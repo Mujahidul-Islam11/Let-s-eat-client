4d9106c99928b4ef091e2cf93eaef03daad1d61b
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-05 20:58:29 +0600

    Navbar responsive design updated

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -4,85 +4,98 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const navLists = (
     <>
-      <Link to="/" className="text-[#000] lg:text-[16px]">
-        <li>Home</li>
-      </Link>
-      <Link to="/contact" className="text-[#000] lg:text-[16px]">
-        <li>Contact Us</li>
-      </Link>
-      <Link to="/menu" className="text-[#000] lg:text-[16px]">
-        <li>Our Menu</li>
-      </Link>
-      <Link to="/shop" className="text-[#000] lg:text-[16px]">
-        <li>Our Shop</li>
-      </Link>
-      <Link to="/dashboard" className="text-[#000] lg:text-[16px]">
-        <li>Dashboard</li>
-      </Link>
+      <li className="hover:text-[#F0C333] transition duration-300">
+        <Link to="/" className="text-[#000] lg:text-[16px]">
+          Home
+        </Link>
+      </li>
+      <li className="hover:text-[#F0C333] transition duration-300">
+        <Link to="/contact" className="text-[#000] lg:text-[16px]">
+          Contact Us
+        </Link>
+      </li>
+      <li className="hover:text-[#F0C333] transition duration-300">
+        <Link to="/menu" className="text-[#000] lg:text-[16px]">
+          Our Menu
+        </Link>
+      </li>
+      <li className="hover:text-[#F0C333] transition duration-300">
+        <Link to="/shop" className="text-[#000] lg:text-[16px]">
+          Our Shop
+        </Link>
+      </li>
+      <li className="hover:text-[#F0C333] transition duration-300">
+        <Link to="/dashboard" className="text-[#000] lg:text-[16px]">
+          Dashboard
+        </Link>
+      </li>
     </>
   );
 
   return (
-    <div className="">
-      <nav className="border rounded-md mt-4 w-full flex justify-between items-center px-6">
-        <div id="nav-1" className="md:w-[25%] flex items-center gap-3">
+    <div className="bg-white shadow-lg mt-4">
+      <nav className="border-b w-full flex justify-between items-center px-4 md:px-6 py-2">
+        {/* nav-1 start*/}
+        <div className="flex items-center">
           {/* Small device menu button */}
-          <div
-            id="smallNav"
-            className="block md:hidden text-3xl mt-7 cursor-pointer"
-          >
+          <div className="block md:hidden text-2xl md:text-3xl cursor-pointer mt-5">
             <ion-icon name="menu-outline"></ion-icon>
           </div>
-          <Link to={"/"}>
+          <Link to={"/"} className="ml-4">
             <img
-              className="w-44 md:w-44 lg:w-56 md:h-24"
+              className="w-32 md:w-44 lg:w-56"
               src="https://imagizer.imageshack.com/img922/4374/m4qNbj.png"
-              alt=""
+              alt="Logo"
             />
           </Link>
         </div>
+        {/* nav-1 end*/}
 
-        <div id="nav-2" className="hidden md:flex items-center w-[50%] h-24">
-          <ul className="flex md:gap-3 lg:gap-10 mt-3">{navLists}</ul>
+        {/* nav-2 start*/}
+        <div className="hidden md:flex justify-center flex-grow">
+          <ul className="flex space-x-4 md:space-x-6 lg:space-x-8 items-center">
+            {navLists}
+          </ul>
         </div>
+        {/* nav-2 end*/}
 
-        <div id="nav-3" className="w-1/4 flex justify-end h-24">
-          <div className="flex items-center gap-5">
-            <div className="text-2xl border border-yellow-200 shadow-md cursor-pointer rounded-full p-3 flex items-center">
-              <ion-icon name="cart-outline"></ion-icon>
-            </div>
-            <div className="dropdown dropdown-end">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle avatar"
-              >
-                <div className="w-16 rounded-full">
-                  <img
-                    alt="Tailwind CSS Navbar component"
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                  />
-                </div>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-              >
-                <li className="">
-                  <h3 className="text-sm text-[#000]">
-                    Mohammad Mojahidul Islam
-                  </h3>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
-              </ul>
-            </div>
-            <button className="hidden rounded-md bg-[#F0C333] hover:bg-opacity-85 py-2 px-6 text-[#000]">
-              Log In
-            </button>
+        {/* nav-3 start*/}
+        <div className="flex items-center space-x-0 md:space-x-4">
+          <div className="text-lg md:text-2xl border border-yellow-200 flex justify-center items-center shadow-md cursor-pointer rounded-full p-2 md:p-3">
+            <ion-icon name="cart-outline"></ion-icon>
           </div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-8 md:w-12 rounded-full">
+                <img
+                  alt="Profile"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <h3 className="text-sm text-[#000]">
+                  Mohammad Mojahidul Islam
+                </h3>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+          <button className="hidden md:inline-block rounded-md bg-[#F0C333] hover:bg-[#E0B227] transition duration-300 py-2 px-6 text-[#000]">
+            Log In
+          </button>
         </div>
+        {/* nav-3 end*/}
       </nav>
     </div>
   );
