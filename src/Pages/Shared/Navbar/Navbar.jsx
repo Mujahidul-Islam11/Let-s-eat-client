5d03a7cfb63486b067d067269ea896f4a4da33c4
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-06 20:14:17 +0600

    Updated the navbar

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = ({openMenu, setOpenMenu}) => {
-  
+const Navbar = ({ openMenu, setOpenMenu }) => {
   const navLists = (
     <>
       <li className="hover:text-[#F0C333] transition duration-300">
@@ -35,79 +34,89 @@ const Navbar = ({openMenu, setOpenMenu}) => {
 
   return (
     <div className="">
-    {/*drawer start - Small device */}
-    {openMenu&&<div className="z-50 w-44 h-40 bg-base-100 absolute top-20 left-3 shadow-md px-6 py-3">
-      <ul className="space-y-1">
-        {navLists}
-      </ul>
-      </div>}
-    {/*drawer end */}
+      {/*drawer start - Small device */}
+      {openMenu && (
+        <div className="z-50 w-44 h-40 bg-base-100 absolute top-20 left-3 shadow-md px-6 py-3">
+          <ul className="space-y-1">{navLists}</ul>
+        </div>
+      )}
+      {/*drawer end */}
       <div className="bg-white shadow-lg mt-4 mx-4 lg:mx-0">
-      <nav className="border-b w-full flex justify-between items-center px-4 md:px-6 py-2">
-        {/* nav-1 start*/}
-        <div className="flex items-center">
-          {/* Small device menu button */}
-          <div  onClick={(e_) => e_.stopPropagation()} className="block md:hidden text-2xl md:text-3xl cursor-pointer mt-5">
-            {openMenu ? <button onClick={()=> setOpenMenu(false)}><ion-icon name="close-outline"></ion-icon></button>:
-            <button onClick={()=> setOpenMenu(true)}><ion-icon name="menu-outline"></ion-icon></button>}
-          </div>
-          <Link to={"/"} className="">
-            <img
-              className="w-32 md:w-44 lg:w-56"
-              src="https://imagizer.imageshack.com/img922/4374/m4qNbj.png"
-              alt="Logo"
-            />
-          </Link>
-        </div>
-        {/* nav-1 end*/}
-
-        {/* nav-2 start*/}
-        <div className="hidden md:flex justify-center flex-grow">
-          <ul className="flex space-x-4 md:space-x-6 lg:space-x-8 items-center">
-            {navLists}
-          </ul>
-        </div>
-        {/* nav-2 end*/}
-
-        {/* nav-3 start*/}
-        <div className="flex items-center space-x-0 md:space-x-4">
-          <div className="text-lg md:text-2xl border border-yellow-200 flex justify-center items-center shadow-md cursor-pointer rounded-full p-2 md:p-3">
-            <ion-icon name="cart-outline"></ion-icon>
-          </div>
-          <div className="dropdown dropdown-end z-50">
+        <nav className="border-b w-full flex justify-between items-center px-4 md:px-6 py-2">
+          {/* nav-1 start*/}
+          <div className="flex items-center">
+            {/* Small device menu button */}
             <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
+              onClick={(e_) => e_.stopPropagation()}
+              className="block md:hidden text-2xl md:text-3xl cursor-pointer mt-5"
             >
-              <div className="w-8 md:w-12 rounded-full">
-                <img
-                  alt="Profile"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
+              {openMenu ? (
+                <button onClick={() => setOpenMenu(false)}>
+                  <ion-icon name="close-outline"></ion-icon>
+                </button>
+              ) : (
+                <button onClick={() => setOpenMenu(true)}>
+                  <ion-icon name="menu-outline"></ion-icon>
+                </button>
+              )}
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <h3 className="text-sm text-[#000]">
-                  Mohammad Mojahidul Islam
-                </h3>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
+            <Link to={"/"} className="">
+              <img
+                className="w-32 md:w-44 lg:w-56"
+                src="https://imagizer.imageshack.com/img922/4374/m4qNbj.png"
+                alt="Logo"
+              />
+            </Link>
+          </div>
+          {/* nav-1 end*/}
+
+          {/* nav-2 start*/}
+          <div className="hidden md:flex justify-center flex-grow">
+            <ul className="flex space-x-4 md:space-x-6 lg:space-x-8 items-center">
+              {navLists}
             </ul>
           </div>
-          <button className="hidden md:inline-block rounded-md bg-[#F0C333] hover:bg-[#E0B227] transition duration-300 py-2 px-6 text-[#000]">
-            Log In
-          </button>
-        </div>
-        {/* nav-3 end*/}
-      </nav>
-    </div>
+          {/* nav-2 end*/}
+
+          {/* nav-3 start*/}
+          <div className="flex items-center space-x-0 md:space-x-4">
+            <div className="text-lg md:text-2xl border text-red-400 flex justify-center items-center shadow-md cursor-pointer rounded-full p-2 md:p-3">
+              <ion-icon name="heart-outline"></ion-icon>
+            </div>
+            <div className="dropdown dropdown-end z-50">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-8 md:w-12 rounded-full">
+                  <img
+                    alt="Profile"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <h3 className="text-sm text-[#000]">
+                    Mohammad Mojahidul Islam
+                  </h3>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+            <button className="hidden rounded-md bg-yellow-400 hover:bg-yellow-500 transition duration-300 py-2 px-6 text-black">
+              Log In
+            </button>
+          </div>
+          {/* nav-3 end*/}
+        </nav>
+      </div>
     </div>
   );
 };
