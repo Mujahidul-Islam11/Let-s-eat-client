4456f24517cfa3c8e5c9a684cee37287a8dedd3e
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-06 11:48:58 +0600

    Created landing page about section

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../Pages/Shared/Navbar/Navbar';
-import Footer from '../Pages/Shared/Footer/Footer';
+import React, { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+import Footer from "../Pages/Shared/Footer/Footer";
+import LocomotiveScroll from "locomotive-scroll";
 
 const Main = () => {
-    const [openMenu, setOpenMenu] = useState(false);
-    return (
-        <div onClick={()=> setOpenMenu(false)} className='relative'>
-            <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu}></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  const [openMenu, setOpenMenu] = useState(false);
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#Landing_page'),
+        smooth: true, 
+    });
+
+    return () => {
+        scroll.destroy();
+    };
+}, []);
+
+  return (
+    <div onClick={() => setOpenMenu(false)} className="relative">
+      <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu}></Navbar>
+      <div id="Landing_page">
+      <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
--- src/Pages/Home/AboutUs/AboutUs.jsx
+++ src/Pages/Home/AboutUs/AboutUs.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const AboutUs = () => {
-    return (
-        <div className='w-full h-[600px] my-12'>
-            <img className='w-[40%] h-[88%] object-cover' src="https://images.unsplash.com/photo-1551218808-94e220e084d2?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-            <div>
-
-            </div>
+  return (
+    <div className="w-full h-[600px] my-12 flex justify-center items-center gap-14">
+      <img
+        className="w-[40%] h-[88%] object-cover"
+        src="https://images.unsplash.com/photo-1551218808-94e220e084d2?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+      <div className="relative w-[40%] h-[88%] bg-white p-8 rounded-md flex flex-col justify-center">
+        <div className="absolute h-52 w-40 bg-gray-300 top-8 z-5 opacity-30 -left-1"></div>
+        <div className="z-10">
+          <h2 className="text-4xl font-bold mb-4">About Us</h2>
+          <p className="text-gray-700 text-lg mb-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Proin eget tortor
+            risus. Nulla quis lorem ut libero malesuada feugiat.
+          </p>
         </div>
-    );
+        <button className="bg-yellow-400 font-extralight py-3 px-6 rounded-md hover:bg-yellow-500 transition-all size-fit shadow-md">
+          Read More
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
