bd0da3f4cb18fffe960b72d7ed122083cf7e31e9
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-06 11:30:57 +0600

    Installed locomotive scroll

---
+++ src/Pages/Home/AboutUs/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div className='w-full h-[600px] my-12'>
+            <img className='w-[40%] h-[88%] object-cover' src="https://images.unsplash.com/photo-1551218808-94e220e084d2?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default AboutUs;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import AboutUs from '../AboutUs/AboutUs';
 
 const Home = () => {
     return (
         <div className=' w-full'>
            <Banner></Banner>
+           <AboutUs></AboutUs>
         </div>
     );
 };
