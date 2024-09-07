36c3bd526db19157dc2f54f957738a76611cd80a
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-07 13:22:11 +0600

    Created banner component and banner layout for contact us page

--- src/Pages/Contact/ContactUs/ContactUs.jsx
+++ src/Pages/Contact/ContactUs/ContactUs.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ContactBanner from '../ContactBanner/ContactBanner';
 
 const ContactUs = () => {
     return (
-        <div>
-            
+        <div className='h-screen'>
+            <ContactBanner></ContactBanner>
         </div>
     );
 };
