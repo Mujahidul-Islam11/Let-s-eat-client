2d90e9c24eb2d5492777e35209320c027bb6be97
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-08 12:32:47 +0600

    Created error page and error route for the platform

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom"
 import Main from "../Layout/Main"
 import Home from "../Pages/Home/Home/Home"
 import ContactUs from "../Pages/Contact/ContactUs/ContactUs"
+import Error from "../Pages/Error/Error"
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <Error></Error>,
       children: [
         {
             path: "/",
