77d6c1e92383201abb0f0ca782b5f560166643c4
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-08 19:43:06 +0600

    Created a useMenu hook to load menu data

--- src/Pages/Home/OurMenu/OurMenu.jsx
+++ src/Pages/Home/OurMenu/OurMenu.jsx
@@ -1,16 +1,11 @@
 import React, { useEffect, useState } from "react";
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 import { NavLink } from "react-router-dom";
+import useMenu from "../../../hooks/useMenu";
 
 const OurMenu = () => {
-  const [menu, setMenu] = useState([]);
   const [filteredMenu, setFilteredMenu] = useState([]);
-  useEffect(() => {
-    fetch("/menu.json")
-      .then((res) => res.json())
-      .then((data) => setMenu(data));
-  }, []);
-
+  const [menu, loading] = useMenu();
   useEffect(() => {
     const popularMenu = menu?.filter((item) => item.status === "popular");
     setFilteredMenu(popularMenu);
@@ -24,28 +19,37 @@ const OurMenu = () => {
           desc={"Explore our delicious offerings."}
         ></SectionTitle>
       </div>
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 px-4 md:mx-0">
-        {filteredMenu?.map((item) => (
-          <div
-            key={item?.name}
-            className="flex flex-col items-center text-center"
-          >
-            <img
-              className="w-40 h-40 mb-4 rounded-full object-cover"
-              src={item?.img}
-              alt={item?.name}
-            />
-            <h3 className="text-lg font-semibold">{item?.name}</h3>
-            <p>{item?.desc}</p>
-            <p className="font-bold text-lg">${item?.price}</p>
-            <p className="text-yellow-500">{item?.rating} ★</p>
-          </div>
-        ))}
+      {loading ? (
+        <div className="text-center flex justify-center py-12">
+        <svg viewBox="0 0 50 50">
+        <circle class="ring" cx="25" cy="25" r="20"></circle>
+        <circle class="ball" cx="25" cy="5" r="3.5"></circle>
+      </svg>
       </div>
+      ) : (
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 px-4 md:mx-0">
+          {filteredMenu?.map((item) => (
+            <div
+              key={item?.name}
+              className="flex flex-col items-center text-center"
+            >
+              <img
+                className="w-40 h-40 mb-4 rounded-full object-cover"
+                src={item?.img}
+                alt={item?.name}
+              />
+              <h3 className="text-lg font-semibold">{item?.name}</h3>
+              <p>{item?.desc}</p>
+              <p className="font-bold text-lg">${item?.price}</p>
+              <p className="text-yellow-500">{item?.rating} ★</p>
+            </div>
+          ))}
+        </div>
+      )}
       <NavLink to={"/menu"}>
-      <button className="bg-yellow-400 font-extralight py-3 px-6 rounded-full hover:bg-yellow-500 transition-all size-fit shadow-md mx-auto flex justify-center mt-6">
-        View More
-      </button>
+        <button className="bg-yellow-400 font-extralight py-3 px-6 rounded-full hover:bg-yellow-500 transition-all size-fit shadow-md mx-auto flex justify-center mt-6">
+          View More
+        </button>
       </NavLink>
     </section>
   );
