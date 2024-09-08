5b2ff287f6d42498611bd863035a405b1ccc4187
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-08 18:53:46 +0600

    made the error page responsive

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center flex-col">
+    <div className="w-full flex justify-center items-center flex-col">
       <img
         className="md:w-1/2 md:h-1/2 object-cover"
         src="https://imagizer.imageshack.com/img923/6628/uY8bZ4.png"
         alt=""
       />
       <div className="text-center space-y-5">
-      <h3 className="text-4xl font-extrabold">
+      <h3 className="text-3xl md:text-4xl font-extrabold">
         Oops! Something went wrong 😥
       </h3>
       <p className="text-sm">
