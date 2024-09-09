86165db86829ce134a96d1111a4ba8f4c5c48f07
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-09 12:21:36 +0600

    Added title and description for menu page

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MenuBanner from '../MenuBanner/MenuBanner';
+import MenuContainer from '../MenuContainer/MenuContainer';
 
 const Menu = () => {
     return (
         <div>
             <MenuBanner></MenuBanner>
+            <MenuContainer></MenuContainer>
         </div>
     );
 };
--- src/Pages/Menu/MenuContainer/MenuContainer.jsx
+++ src/Pages/Menu/MenuContainer/MenuContainer.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 
 const MenuContainer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="my-12 py-10">
+      <SectionTitle
+        title={"Savor the Flavor"}
+        desc={
+          "Fresh, delicious meals for every craving. Discover your new favorite today!"
+        }
+      ></SectionTitle>
+    </div>
+  );
 };
 
 export default MenuContainer;
