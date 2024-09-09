2a961a283a4f5f5ff07d8ae259378947cdcdca79
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-09 20:07:08 +0600

    Updated the json data

--- src/Pages/Menu/MenuContainer/MenuContainer.jsx
+++ src/Pages/Menu/MenuContainer/MenuContainer.jsx
@@ -1,6 +1,7 @@
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 import PopularMenu from "../MenuCategories/MenuCategories";
 import useMenu from "../../../hooks/useMenu";
+import { NavLink } from "react-router-dom";
 
 const MenuContainer = () => {
   const [menu, loading] = useMenu();
@@ -13,9 +14,29 @@ const MenuContainer = () => {
           "Fresh, delicious meals for every craving. Discover your new favorite today!"
         }
       />
-      <PopularMenu menu={menu} loading={loading} category={"Main Course"} title={"Main Course"}></PopularMenu>
-      <PopularMenu menu={menu} loading={loading} category={"Salads"} title={"Salads"}></PopularMenu>
-      <PopularMenu menu={menu} loading={loading} category={"Beverages"} title={"Beverages"}></PopularMenu>
+      <PopularMenu
+        menu={menu}
+        loading={loading}
+        category={"Chef's Specials"}
+        title={"Chef's Specials"}
+      ></PopularMenu>
+      <PopularMenu
+        menu={menu}
+        loading={loading}
+        category={"Salads"}
+        title={"Salads"}
+      ></PopularMenu>
+      <PopularMenu
+        menu={menu}
+        loading={loading}
+        category={"Beverages"}
+        title={"Beverages"}
+      ></PopularMenu>
+      <NavLink to={"/shop"}>
+      <button className="bg-yellow-400 font-extralight py-3 px-6 rounded-full hover:bg-yellow-500 transition-all size-fit shadow-md flex justify-center mx-auto mt-6">
+        Order Now
+      </button>
+      </NavLink>
     </div>
   );
 };
