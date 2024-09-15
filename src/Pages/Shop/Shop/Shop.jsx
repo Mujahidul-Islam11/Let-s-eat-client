41a9db428b07fc5391bccd970e8749dd2c45fa8a
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-15 19:22:02 +0600

    Added dynamic banner to menu and shop page

--- src/Pages/Shop/Shop/Shop.jsx
+++ src/Pages/Shop/Shop/Shop.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ShopBanner from '../ShopBanner/ShopBanner';
+import SectionBanner from '../../Shared/SectionBanner/SectionBanner';
 
 const Shop = () => {
     return (
         <div>
-            <ShopBanner></ShopBanner>
+            <SectionBanner title={"Our Shop"} pageName={"Shop"} url={"https://images.unsplash.com/photo-1637447776710-e67450d2efaf?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}></SectionBanner>
         </div>
     );
 };
