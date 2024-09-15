206139356a0856b227a5cd7680761e8cc7359f79
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-15 18:58:19 +0600

    Created dynamic component and imported on contactus page

--- src/Pages/Contact/ContactUs/ContactUs.jsx
+++ src/Pages/Contact/ContactUs/ContactUs.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ContactBanner from '../ContactBanner/ContactBanner';
 import ContactInfo from '../ContactInfo/ContactInfo';
 import Location from '../Location/Location';
+import SectionBanner from '../../Shared/SectionBanner/SectionBanner';
 
 const ContactUs = () => {
     return (
         <div className=''>
-            <ContactBanner></ContactBanner>
+            <SectionBanner title={"Contact Us"} pageName={"Contact"} url={'https://images.unsplash.com/photo-1505935428862-770b6f24f629?q=80&w=1467&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}></SectionBanner>
             <ContactInfo></ContactInfo>
             <Location></Location>
         </div>
--- src/Pages/Shared/SectionBanner/SectionBanner.jsx
+++ src/Pages/Shared/SectionBanner/SectionBanner.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const SectionBanner = () => {
+const SectionBanner = ({url, title, pageName}) => {
     return (
-        <div>
-            
+        <div
+      className="w-full h-[200px] md:h-[300px]"
+      style={{
+        backgroundImage: `url('${url}')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="w-full h-full bg-black bg-opacity-20 text-center flex flex-col justify-center items-center gap-4">
+        <h3 className="text-3xl md:text-5xl font-semibold">{title}</h3>
+        <div className="flex gap-3">
+            <NavLink to={"/"}><h3 className="text-sm md:text-lg hover:text-yellow-300 duration-300">Home</h3></NavLink>
+            <h3 className="text-4xl -mt-4 text-yellow-300">.</h3>
+            <h3 className="text-sm md:text-lg border-b border-yellow-300">{pageName}</h3>
         </div>
+      </div>
+    </div>
     );
 };
 
