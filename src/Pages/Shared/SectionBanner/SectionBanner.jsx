29a6c14e92c33bdcf577a22f873dde92c45671ff
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-16 09:33:00 +0600

    Created filtering buttons for shoping page

--- src/Pages/Shared/SectionBanner/SectionBanner.jsx
+++ src/Pages/Shared/SectionBanner/SectionBanner.jsx
@@ -11,12 +11,12 @@ const SectionBanner = ({url, title, pageName}) => {
         backgroundPosition: "center",
       }}
     >
-      <div className="w-full h-full bg-black bg-opacity-10 text-center flex flex-col justify-center items-center gap-4">
+      <div className="w-full h-full bg-black bg-opacity-15 text-center flex flex-col justify-center items-center gap-4">
         <h3 className="text-3xl md:text-5xl font-semibold">{title}</h3>
         <div className="flex gap-3">
             <NavLink to={"/"}><h3 className="text-sm md:text-lg hover:text-yellow-300 duration-300">Home</h3></NavLink>
             <h3 className="text-4xl -mt-4 text-yellow-300">.</h3>
-            <h3 className="text-sm md:text-lg border-b border-yellow-300">{pageName}</h3>
+            <h3 className="text-sm md:text-lg border-b-2 border-yellow-300">{pageName}</h3>
         </div>
       </div>
     </div>
--- src/Pages/Shop/Shop/Shop.jsx
+++ src/Pages/Shop/Shop/Shop.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import SectionBanner from '../../Shared/SectionBanner/SectionBanner';
+import React, { useState } from "react";
+import SectionBanner from "../../Shared/SectionBanner/SectionBanner";
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 
 const Shop = () => {
-    return (
-        <div>
-            <SectionBanner title={"Our Shop"} pageName={"Shop"} url={"https://images.unsplash.com/photo-1637447776710-e67450d2efaf?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}></SectionBanner>
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const categoryBtns = (
+    <>
+      <button onClick={()=> setSelectedCategory("All")} className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${selectedCategory === "All"? 'bg-yellow-400': 'bg-white'} transition-all size-fit shadow-md flex justify-center mt-6`}>
+        All
+      </button>
+      <button onClick={()=> setSelectedCategory("Chef's Specials")} className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${selectedCategory === "Chef's Specials"? 'bg-yellow-400': 'bg-white'} transition-all size-fit shadow-md flex justify-center mt-6`}>
+        Chef's Specials
+      </button>
+      <button onClick={()=> setSelectedCategory("Salads")} className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${selectedCategory === "Salads"? 'bg-yellow-400': 'bg-white'} transition-all size-fit shadow-md flex justify-center mt-6`}>
+        Salads
+      </button>
+      <button onClick={()=> setSelectedCategory("Beverages")} className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${selectedCategory === "Beverages"? 'bg-yellow-400': 'bg-white'} transition-all size-fit shadow-md flex justify-center mt-6`}>
+        Beverages
+      </button>
+    </>
+  );
+  return (
+    <div>
+      <SectionBanner
+        title={"Our Shop"}
+        pageName={"Shop"}
+        url={
+          "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+      ></SectionBanner>
+      <div className="p-6 md:p-10 my-12">
+        <SectionTitle
+          title={" Explore Our Menu 🍴"}
+          desc={
+            "Delicious meals, fresh ingredients. Browse, pick, and enjoy your favorites!"
+          }
+        ></SectionTitle>
+        <div className="flex justify-center gap-4">
+            {categoryBtns}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
