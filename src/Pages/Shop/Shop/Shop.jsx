808ed9ca3cbd4c28b040f6be376778f0b8502789
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-16 11:29:27 +0600

    Updated shoping cards container

--- src/Pages/Shop/Shop/Shop.jsx
+++ src/Pages/Shop/Shop/Shop.jsx
@@ -80,7 +80,7 @@ const Shop = () => {
         {/* menu cards starts*/}
         <div className="flex justify-center">
           {loading ? (
-            <div className="text-center flex justify-center">
+            <div className="text-center flex justify-center mb-24">
               <svg id="svgStyle" viewBox="0 0 50 50">
                 <circle class="ring" cx="25" cy="25" r="20"></circle>
                 <circle class="ball" cx="25" cy="5" r="3.5"></circle>
