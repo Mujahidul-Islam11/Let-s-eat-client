fbd6f964266fd28fbbd1b2696912a0397612839e
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-16 10:05:11 +0600

    Created filtering functionality for shop page

--- src/Pages/Shop/Shop/Shop.jsx
+++ src/Pages/Shop/Shop/Shop.jsx
@@ -1,25 +1,64 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SectionBanner from "../../Shared/SectionBanner/SectionBanner";
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
 
 const Shop = () => {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [filteredMenu, setFilteredMenu] = useState([]);
+  const [menu, loading] = useMenu();
+
   const categoryBtns = (
     <>
-      <button onClick={()=> setSelectedCategory("All")} className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${selectedCategory === "All"? 'bg-yellow-400': 'bg-white'} transition-all size-fit shadow-md flex justify-center mt-6`}>
+      <button
+        onClick={() => setSelectedCategory("All")}
+        className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${
+          selectedCategory === "All" ? "bg-yellow-400" : "bg-white"
+        } transition-all size-fit shadow-md flex justify-center mt-6`}
+      >
         All
       </button>
-      <button onClick={()=> setSelectedCategory("Chef's Specials")} className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${selectedCategory === "Chef's Specials"? 'bg-yellow-400': 'bg-white'} transition-all size-fit shadow-md flex justify-center mt-6`}>
+      <button
+        onClick={() => setSelectedCategory("Chef's Specials")}
+        className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${
+          selectedCategory === "Chef's Specials" ? "bg-yellow-400" : "bg-white"
+        } transition-all size-fit shadow-md flex justify-center mt-6`}
+      >
         Chef's Specials
       </button>
-      <button onClick={()=> setSelectedCategory("Salads")} className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${selectedCategory === "Salads"? 'bg-yellow-400': 'bg-white'} transition-all size-fit shadow-md flex justify-center mt-6`}>
+      <button
+        onClick={() => setSelectedCategory("Salads")}
+        className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${
+          selectedCategory === "Salads" ? "bg-yellow-400" : "bg-white"
+        } transition-all size-fit shadow-md flex justify-center mt-6`}
+      >
         Salads
       </button>
-      <button onClick={()=> setSelectedCategory("Beverages")} className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${selectedCategory === "Beverages"? 'bg-yellow-400': 'bg-white'} transition-all size-fit shadow-md flex justify-center mt-6`}>
+      <button
+        onClick={() => setSelectedCategory("Beverages")}
+        className={` border font-extralight py-3 px-6 hover:bg-yellow-400 rounded-full ${
+          selectedCategory === "Beverages" ? "bg-yellow-400" : "bg-white"
+        } transition-all size-fit shadow-md flex justify-center mt-6`}
+      >
         Beverages
       </button>
     </>
   );
+
+  // data filtering starts
+  useEffect(() => {
+    if (selectedCategory == "All") {
+      setFilteredMenu(menu);
+    } else {
+      const filteredData = menu?.filter(
+        (mnEl) => mnEl?.category === selectedCategory
+      );
+      setFilteredMenu(filteredData);
+    }
+  }, [menu, selectedCategory]);
+  // data filtering ends
+
+  console.log(filteredMenu);
   return (
     <div>
       <SectionBanner
@@ -36,9 +75,24 @@ const Shop = () => {
             "Delicious meals, fresh ingredients. Browse, pick, and enjoy your favorites!"
           }
         ></SectionTitle>
-        <div className="flex justify-center gap-4">
-            {categoryBtns}
-        </div>
+        <div className="flex justify-center gap-4">{categoryBtns}</div>
+        {/* menu cards */}
+        {loading ? (
+          <div className="text-center flex justify-center py-12">
+            <svg id="svgStyle" viewBox="0 0 50 50">
+              <circle class="ring" cx="25" cy="25" r="20"></circle>
+              <circle class="ball" cx="25" cy="5" r="3.5"></circle>
+            </svg>
+          </div>
+        ) : (
+          <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 px-4 md:mx-0 mt-12">
+            {filteredMenu?.map((items) => (
+              <div>
+                <h3>{items?.category}</h3>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
