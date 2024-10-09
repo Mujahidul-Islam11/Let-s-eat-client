327bdd79ecfc9de818861c26a354256249a84983
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-10-09 20:40:04 +0600

    Created post method for adding items to favorites

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,21 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 import Footer from "../Pages/Shared/Footer/Footer";
-import LocomotiveScroll from "locomotive-scroll";
 import { Toaster } from "sonner";
 
 const Main = () => {
   const [openMenu, setOpenMenu] = useState(false);
-//   useEffect(() => {
-//     const scroll = new LocomotiveScroll({
-//         el: document.querySelector('#Landing_page'),
-//         smooth: true, 
-//     });
-
-//     return () => {
-//         scroll.destroy();
-//     };
-// }, []);
 
   return (
     <div onClick={() => setOpenMenu(false)} className=" relative lg:max-w-7xl lg:mx-auto">
--- src/Pages/Shop/Shop/ItemCard.jsx
+++ src/Pages/Shop/Shop/ItemCard.jsx
@@ -1,22 +1,45 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../../provider/AuthProvider";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "sonner";
 
 const ItemCard = ({ item }) => {
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const handleAddToList = (favItem) => {
-    if(user && user.email){
-      // send fav items to db
-    }else{
-      navigate("/login")
+  const handleAddToList = (food) => {
+    if (user && user.email) {
+      const favItem = {
+        menuId: item._id,
+        email: user.email,
+        name: item.name,
+        img: item.img,
+        price: item.price,
+      };
+
+      // send data to database
+      axios
+        .post("http://localhost:5000/favorites", favItem)
+        .then((res) => {
+          if (res.data.insertedId) {
+            toast.success(`${item.name} added to the favorites`);
+          } else {
+            toast.error("Oops! Something went wrong");
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          toast.error("Oops! Something went wrong");
+        });
+    } else {
+      navigate("/login");
     }
   };
   return (
     <div
       key={item?.name}
-      className="flex flex-col items-center text-center hover:scale-105 duration-300 cursor-pointer py-3 px-4 rounded-md shadow-xl"
+      className="flex flex-col items-center text-center cursor-pointer py-3 px-4 rounded-md shadow-xl"
     >
       <img
         className="w-36 h-36 mb-4 rounded-full object-cover -mt-20"
@@ -26,7 +49,7 @@ const ItemCard = ({ item }) => {
       <div className="text-center space-y-2">
         <div className="flex justify-between px-6 items-center">
           <p className="text-yellow-500">{item?.rating} ★</p>
-          <button onClick={()=>handleAddToList(item)} className="text-3xl">
+          <button onClick={() => handleAddToList(item)} className="text-3xl hover:text-red-500 hover:scale-125 duration-300">
             <ion-icon name="heart-outline"></ion-icon>
           </button>
         </div>
