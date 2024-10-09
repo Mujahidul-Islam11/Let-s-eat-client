0ebcef60a3d30705a41320b7e92a2ee879563fd2
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-10-09 21:13:45 +0600

    Loaded favorite items data using tanstack query and axios

--- src/hooks/useFavorites.jsx
+++ src/hooks/useFavorites.jsx
@@ -1,11 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
 
 
 const useFavorites = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const axiosSecure = useAxiosSecure();
+
+// fetch data with tanStack query
+  const { data: favItems = [] } = useQuery({
+    queryKey: ["favData"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/favorites");
+      return res.data;
+    },
+  });
+
+  return [favItems];
 };
 
 export default useFavorites;
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,21 @@ import {
 import { router } from "/src/routes/Routes";
 import "./index.css";
 import AuthProvider from "./provider/AuthProvider";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider>
     <RouterProvider router={router} />
     </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
