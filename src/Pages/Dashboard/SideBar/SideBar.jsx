75d5e11829b99b544e6440ac2b60ae0c4502f82c
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-10-14 19:30:41 +0600

    Updated dashboard sidebar

--- src/Pages/Dashboard/SideBar/SideBar.jsx
+++ src/Pages/Dashboard/SideBar/SideBar.jsx
@@ -2,49 +2,71 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="h-screen w-64 bg-white shadow-lg">
-      <ul className="">
+    <div className="h-screen w-64 bg-white shadow-lg flex flex-col justify-between">
+      <div>
+      <ul className="py-6 px-4">
         <Link>
-          <li className="text-lg flex items-center gap-3 hover:bg-gray-100 py-3 px-6 group transition-all">
-            <span className="group-hover:text-yellow-500 duration-300 mt-1 text-gray-600">
+          <li className="text-lg rounded-md flex items-center gap-3 hover:bg-[#F7F8FA] py-3 px-6 group transition-all">
+            <span className="group-hover:text-yellow-500 duration-200 mt-1 text-gray-600">
               <ion-icon name="grid-outline"></ion-icon>
             </span>
             <span className="text-gray-700">Dashboard</span>
           </li>
         </Link>
         <Link>
-          <li className="text-lg flex items-center gap-3 hover:bg-gray-100 py-3 px-6 group transition-all">
-            <span className="group-hover:text-yellow-500 duration-300 mt-1 text-gray-600">
+          <li className="text-lg rounded-md flex items-center gap-3 hover:bg-[#F7F8FA] py-3 px-6 group transition-all">
+            <span className="group-hover:text-yellow-500 duration-200 mt-1 text-gray-600">
               <ion-icon name="calendar-clear-outline"></ion-icon>
             </span>
             <span className="text-gray-700">Reservation</span>
           </li>
         </Link>
         <Link>
-          <li className="text-lg flex items-center gap-3 hover:bg-gray-100 py-3 px-6 group transition-all">
-            <span className="group-hover:text-yellow-500 duration-300 mt-1 text-gray-600">
+          <li className="text-lg rounded-md flex items-center gap-3 hover:bg-[#F7F8FA] py-3 px-6 group transition-all">
+            <span className="group-hover:text-yellow-500 duration-200 mt-1 text-gray-600">
               <ion-icon name="heart-outline"></ion-icon>
             </span>
             <span className="text-gray-700">My Favorites</span>
           </li>
         </Link>
         <Link>
-          <li className="text-lg flex items-center gap-3 hover:bg-gray-100 py-3 px-6 group transition-all">
-            <span className="group-hover:text-yellow-500 duration-300 mt-1 text-gray-600">
+          <li className="text-lg rounded-md flex items-center gap-3 hover:bg-[#F7F8FA] py-3 px-6 group transition-all">
+            <span className="group-hover:text-yellow-500 duration-200 mt-1 text-gray-600">
               <ion-icon name="star-outline"></ion-icon>
             </span>
             <span className="text-gray-700">Add a Review</span>
           </li>
         </Link>
         <Link>
-          <li className="text-lg flex items-center gap-3 hover:bg-gray-100 py-3 px-6 group transition-all">
-            <span className="group-hover:text-yellow-500 duration-300 mt-1 text-gray-600">
+          <li className="text-lg rounded-md flex items-center gap-3 hover:bg-[#F7F8FA] py-3 px-6 group transition-all">
+            <span className="group-hover:text-yellow-500 duration-200 mt-1 text-gray-600">
               <ion-icon name="clipboard-outline"></ion-icon>
             </span>
             <span className="text-gray-700">My Bookings</span>
           </li>
         </Link>
       </ul>
+      </div>
+      <div>
+      <ul className="py-6 px-4">
+        <Link>
+          <li className="text-lg rounded-md flex items-center gap-3 hover:bg-[#F7F8FA] py-3 px-6 group transition-all">
+            <span className="group-hover:text-yellow-500 duration-200 mt-1 text-gray-600">
+              <ion-icon name="settings-outline"></ion-icon>
+            </span>
+            <span className="text-gray-700">Settings</span>
+          </li>
+        </Link>
+        <Link>
+          <li className="text-lg rounded-md flex items-center gap-3 hover:bg-[#F7F8FA] py-3 px-6 group transition-all">
+            <span className="group-hover:text-yellow-500 duration-200 mt-1 text-gray-600">
+              <ion-icon name="log-out-outline"></ion-icon>
+            </span>
+            <span className="text-gray-700">Logout</span>
+          </li>
+        </Link>
+      </ul>
+      </div>
     </div>
   );
 };
