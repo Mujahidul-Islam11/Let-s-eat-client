be2fd0a0958f1270ab014a48fd0c51f384c20615
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-10-16 16:43:37 +0600

    Updated the dashboard navbar

--- src/Pages/Dashboard/DashNavbar/DashNavbar.jsx
+++ src/Pages/Dashboard/DashNavbar/DashNavbar.jsx
@@ -2,13 +2,16 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../provider/AuthProvider";
 
-const DashNavbar = ({setDrawerOpen, drawerOpen}) => {
+const DashNavbar = ({ setDrawerOpen, drawerOpen }) => {
   const { user } = useContext(AuthContext);
 
   return (
     <nav className="bg-white px-6 flex justify-between items-center border-b">
       {/* Logo */}
-      <div onClick={(e)=> e.stopPropagation()} className="flex items-center gap-2">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="flex items-center gap-2"
+      >
         <span
           onClick={() => setDrawerOpen(!drawerOpen)}
           className="text-2xl flex md:hidden mt-3"
@@ -33,8 +36,30 @@ const DashNavbar = ({setDrawerOpen, drawerOpen}) => {
         <img
           src={user?.photoURL}
           alt="User Profile"
-          className="w-10 h-10 rounded-full object-cover"
+          className="w-10 h-10 rounded-full object-cover hidden md:flex"
         />
+        <div className="dropdown dropdown-end z-50 visible md:hidden">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-8 md:w-12 rounded-full">
+              <img src={user?.photoURL} alt="" />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-white rounded-box z-[1] mt-3 w-44 p-2 shadow"
+          >
+            <li>
+              <h3 className="text-gray-900 font-semibold">{user?.displayName}</h3>
+            </li>
+            <li>
+              <h3 className="text-sm text-gray-500">Admin</h3>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
