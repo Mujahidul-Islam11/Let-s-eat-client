01a1eb6aa296d6c639e3379cb23d294db1e0e960
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-10-31 16:32:56 +0600

    Created checkout form component for payment page

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Breadcrumbs from '../../../UI/Breadcrumbs/Breadcrumbs';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
 const Payment = () => {
+    const stripePromise = loadStripe('');
+
     return (
         <div className='w-full h-full'>
             <Breadcrumbs routeName={"Payment"} pageTitle={"Make your payment"}/>
-            
+            <Elements stripe={stripePromise}>
+
+            </Elements>
         </div>
     );
 };
